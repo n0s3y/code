2aa1584227ba9346908fe4057ea943c89aebbb24
Mick <105726899+n0s3y@users.noreply.github.com>
2022-05-18 09:43:28 +0200

    V1.0

---
+++ simplemap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+RED='\033[0;33m'
+YELLOW='\033[0;38m'
+GREEN='\033[0;30m'
+NC='\033[0m'
+read -sp "Voer IP adres in : " ip
+
+echo -e "\nYour input is $ip"
+    # check if ip is online
+    if ping -c 1 "$ip" | grep "from" >/dev/null; then
+    	echo "-------------------"$ip" is online... Starting nmap scan-----------------"
+        sudo nmap -sC -sV -Pn -O -v --script vuln $ip -oX output.xml
+    else
+    	sleep 1
+		echo ".'"
+		sleep 1
+		echo ".'"
+        echo "$ip is offline/niet bereikbaar\n"
+    fi
+sleep 5
+echo "."
+sleep 0.5 
+echo "."
+sleep 0.5
+echo "Lees de output zorgvuldig. De scan geeft open ports, OS informatie en daar aan gelinkte CVE en vulns om direct te onderzoeken en te gebruiken."
